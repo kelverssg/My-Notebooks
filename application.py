4173b45fd6e7fd0fa195e95cf8c6cddc59a5c384
Kelvin Tan <kelvers@kelvins-mbp.lan>
2020-04-21 23:51:30 +0800

    Python flask learnings

--- application.py
+++ application.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from flask_session.__init__ import Session
+from datetime import datetime
 
 app = Flask(__name__)
+
+app.config['SESSION_PERMANENT'] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route('/')
 def index():
     headline = 'This is awesome!'
     nums = range(1, 11)
     return render_template('test.html', headline=headline, nums=nums)
+
+@app.route('/notes', methods=["GET", "POST"])
+def le_notes():
+    if request.method == 'POST':
+        if session.get('notes') == None: session['notes'] = []
+        note = request.form.get('note')
+        session['notes'].append(note)
+    return render_template('notes.html', notes=session['notes'])
 
 @app.route('/secondary')
 def secondary():
@@ -19,6 +33,18 @@ def tertiary():
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/second')
+def second():
+    return render_template('second.html')
+
+@app.route('/twentyfifty', methods=["GET","POST"])
+def twentyfifty():
+    if request.method == 'GET': return render_template('main.html')
+    current_year = datetime.now().year
+    age = int(request.form.get("age"))
+    age2050 = age + 2050 - current_year
+    return render_template('twentyfifty.html', current_year=current_year, age=age, age2050=age2050)
 
 @app.route('/<string:name>')
 def generic(name):
