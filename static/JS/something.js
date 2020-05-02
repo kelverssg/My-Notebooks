9e5d0d0b6aff00dfb1bf42dff42c53f9ea889e4f
Kelvin Tan <kelvers@kelvins-mbp.lan>
2020-05-02 21:57:47 +0800

    With JS lesson

---
+++ static/JS/something.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#button_inc').onclick = count;
+    document.querySelector('#button_dec').onclick = decrease;
+    document.querySelector('#form1').onsubmit = function() {
+        const name = document.querySelector('#name1').value;
+        alert(`Hello ${name}!`);
+    };
+});
+
+let counter = 0;
+
+function count() {
+    counter++;
+    document.querySelector('#counter').innerHTML = "Counter: " + counter;
+    if (counter % 10 === 0) {alert(`Counter is at ${counter}!`);}
+
+}
+
+function decrease() {
+    counter--;
+    document.querySelector('#counter').innerHTML = "Counter: " + counter;
+    if (counter % 10 === 0) {alert(`Counter is at ${counter}!`);}
+}
