a33b69c2d7a20317df90970e7986ccf6730fe1eb
Kelvin Tan <kelvers@kelvins-mbp.lan>
2020-05-04 23:11:03 +0800

    Updated something.js

--- static/JS/something.js
+++ static/JS/something.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#button_inc').onclick = count;
     document.querySelector('#button_dec').onclick = decrease;
+    
     document.querySelector('#form1').onsubmit = function() {
         const name = document.querySelector('#name1').value;
         alert(`Hello ${name}!`);
     };
+
+    for (i of ['#orange', '#brown', '#purple']) {
+        document.querySelector(i).onclick = function() {
+            document.querySelector('#colorful').style.color = i.slice(1, id_.length);
+    }
+
     // This is a comment.
 });
 
